c9d9be7631d0f9b62fe837fd3d11e5bdaa2d4a42
ashin000 <ashinpramod010@gmail.com>
2024-05-07 10:30:41 +0530

    conditions checked

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,22 +14,99 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  List<String> viewTap = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ];
+  bool firstTurn = true;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(),
+    return Scaffold(
+      appBar: AppBar(),
       backgroundColor: Colors.white,
       body: GridView.builder(
           itemCount: 9,
           gridDelegate:
               SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
           itemBuilder: (BuildContext context, int index) {
-            return Container(decoration: BoxDecoration(border: Border.all()),
-                child: Center(
-                    child: Text(
-              index.toString(),
-              style: TextStyle(color: Colors.black, fontSize: 30),
-            )));
+            return GestureDetector(
+              onTap: () {
+                _pressed(index);
+              },
+              child: Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  child: Center(
+                      child: Text(
+                    viewTap[index],
+                    //index.toString(),
+                    style: TextStyle(color: Colors.black, fontSize: 30),
+                  ))),
+            );
           }),
     );
   }
+
+  void _pressed(int index) {
+    setState(() {
+      // viewTap[index]="O";
+      if (firstTurn) {
+        viewTap[index] = "X";
+      } else {
+        viewTap[index] = "O";
+        firstTurn = !firstTurn;
+        _checkWin();
+      }
+    });
+  }
+
+  void _checkWin() {
+    if (viewTap[0] == viewTap[1] &&
+        viewTap[0] == viewTap[2] &&
+        viewTap[0] != '') {
+      _showMessage(viewTap[0]);
+    }
+    if (viewTap[3] == viewTap[4] &&
+        viewTap[3] == viewTap[5] &&
+        viewTap[3] != "") {
+      _showMessage(viewTap[3]);
+    }
+    if (viewTap[6] == viewTap[7] && viewTap[6] == viewTap[8] && viewTap != "") {
+      _showMessage(viewTap[6]);
+    }
+    if (viewTap[0] == viewTap[3] && viewTap[0] == viewTap[6] && viewTap != "") {
+      _showMessage(viewTap[0]);
+    }
+    if (viewTap[1] == viewTap[4] && viewTap[1] == viewTap[7] && viewTap != '') {
+      _showMessage(viewTap[1]);
+    }
+    if (viewTap[2] == viewTap[5] && viewTap[1] == viewTap[8] && viewTap != '') {
+      _showMessage(viewTap[2]);
+    }
+    if (viewTap[0] == viewTap[4] && viewTap[0] == viewTap[8] && viewTap != '') {
+      _showMessage(viewTap[0]);
+      if (viewTap[2] == viewTap[4] &&
+          viewTap[2] == viewTap[6] &&
+          viewTap != '') {
+        _showMessage(context);
+      }
+    }
+  }
+}
+
+void _showMessage(context) {
+  showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Won"),
+        );
+      });
 }
